cb7e23a1b33f9bc1230e80869df06ddff4501a21
Serena Gupta <serenagupta@Serena-Guptas-MacBook-Pro.local>
2016-03-02 18:27:30 -0800

    adding in training set

--- testData.py
+++ testData.py
@@ -1,6 +1,35 @@
 import csv
 import random
 
+def getSample():
+    n = 20
+    with open("dataset1.csv") as f:
+        for line in f:
+            arr = line.split(", ")
+        next = []
+        for i, e in enumerate(arr):
+            start = e.find("\r")
+            if start == -1:
+                next.append(e)
+            else:
+                first = e[0:start]
+                last = e[start+1:]
+                if ("\r" not in first) and ("\r" not in last):
+                    next.append(first)
+                    next.append(last)
+                else:
+                    next.append("medimmune.com")
+                    next.append("University of North Carolina at Chapel Hill")
+        compToWeb = {}
+        webToComp = {}
+        isCompany = True
+        for i in range(len(next)):
+            if isCompany:
+                compToWeb[next[i]] = next[i+1]
+                webToComp[next[i+1]] = next[i]
+            isCompany = not isCompany
+        return compToWeb, webToComp
+
 def getNCompanies(n=20):
     n = 20 if n < 1 or n > 947 else int(n)
     with open("companies.txt") as f:
