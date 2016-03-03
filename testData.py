ae23033b4e25a3f6489f322f097c610aa9ae9b5b
Serena Gupta <serenagupta@Serena-Guptas-MacBook-Pro.local>
2016-03-03 01:14:42 -0800

    fixed to split training and testing data

--- testData.py
+++ testData.py
@@ -1,28 +1,33 @@
 import csv
 import random
 
-def getDictionary():
+def getData():
     with open("dataset1.csv") as f:
         for line in f:
             arr = line.replace("\r", "").split(",")[:-1]
-        compToWeb = {}
+        compToWebTrain = {}
+        compToWebTest = {}
         for i in range(len(arr)):
-            if i % 2 == 0:
-                compToWeb[arr[i]] = arr[i+1].strip()
-        return compToWeb
+            if i % 4 == 0:
+                compToWebTrain[arr[i]] = arr[i+1].strip()
+            if i % 4 == 2:
+                compToWebTest[arr[i]] = arr[i+1].strip()
+        return (compToWebTrain, compToWebTest)
 
-def getNCompanies(n=20):
-    n = 20 if n < 1 or n > 947 else int(n)
-    with open("companies.txt") as f:
-        s = random.sample(range(1, 947), n)
-        i = 1
-        arr = []
-        for line in f:
-            if i in s:
-                data = line.strip('\n').replace("\x00", "")[:-1]
-                if data[0] == "\"":
-                    data = data[1:len(data)-1]
-                arr.append(data)
-            i += 1
-    return arr
+companyToWebsiteTrainingDictionary, companyToWebsiteTestingDictionary = getData()
+
+# def getNCompanies(n=20):
+#     n = 20 if n < 1 or n > 947 else int(n)
+#     with open("companies.txt") as f:
+#         s = random.sample(range(1, 947), n)
+#         i = 1
+#         arr = []
+#         for line in f:
+#             if i in s:
+#                 data = line.strip('\n').replace("\x00", "")[:-1]
+#                 if data[0] == "\"":
+#                     data = data[1:len(data)-1]
+#                 arr.append(data)
+#             i += 1
+#     return arr
 
